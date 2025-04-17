d9322dc44b2fada4d8684bdad050b8a288d28eed
Lucas Santos <kelytonlucas@gmail.com>
2025-04-17 01:40:43 -0300

    feat: added func showInfo in emprestimo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import model.Emprestimo;
+import model.Livro;
+import model.Usuario;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Usuario pessoa1 = new Usuario("Santos", "333.333.333-33", "lucassantos@lucas.com");
+        Livro livro = new Livro("1984", "George Orwell", 1949);
+        Emprestimo newEmprestimo = new Emprestimo(livro, pessoa1, 3);
+
+        newEmprestimo.showInfo();
     }
 }
--- src/model/Emprestimo.java
+++ src/model/Emprestimo.java
@@ -3,13 +3,15 @@ package model;
 public class Emprestimo {
     private Livro livro;
     private Usuario usuario;
+    private Integer borrowDays;
 
     public Emprestimo() {
     }
 
-    public Emprestimo(Livro livro, Usuario usuario) {
+    public Emprestimo(Livro livro, Usuario usuario, Integer borrowDays) {
         this.livro = livro;
         this.usuario = usuario;
+        this.borrowDays = borrowDays;
     }
 
     public Livro getLivro() {
@@ -26,6 +28,22 @@ public class Emprestimo {
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+
+    public Integer getDays() {
+        return borrowDays;
+    }
+
+    public void setDays(Integer days) {
+        this.borrowDays = days;
+    }
+
+    public void showInfo() {
+        System.out
+                .println("The user is: " + getUsuario().getName() + " with associated email: " + getUsuario().getEmail()
+                        + " subscribed with the CPF: " + getUsuario().getCpf() + " has borrowed the book : "
+                        + getLivro().getTitle() +
+                        " released in: " + getLivro().getReleaseYear() + " by the author: " + getLivro().getAuthor());
     }
 
 }
