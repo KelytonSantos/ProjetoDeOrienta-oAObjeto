3d10c2087fef216bcee59692a21839394bc147af
Lucas Santos <kelytonlucas@gmail.com>
2025-04-17 01:28:31 -0300

    feat: added usuario, emprestimo and livro class

---
+++ src/model/Emprestimo.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Emprestimo {
+    private Livro livro;
+    private Usuario usuario;
+
+    public Emprestimo() {
+    }
+
+    public Emprestimo(Livro livro, Usuario usuario) {
+        this.livro = livro;
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+}
