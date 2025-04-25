f6f7ccf686102f6fb3cb25be4a9d84d2c8877922
Lucas Santos <kelytonlucas@gmail.com>
2025-04-25 11:56:50 -0300

    feat: Adding attributes and methods to class Classe

--- src/model/Classe.java
+++ src/model/Classe.java
@@ -1,5 +1,45 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Classe {
 
+    private String nome;
+    private Integer cargaHoraria;
+    private List<Turma> turma = new ArrayList<>();
+
+    public Classe() {
+        this.nome = "";
+        this.cargaHoraria = 0;
+    }
+
+    public Classe(String nome, Integer cargaHoraria) {
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public List<Turma> getTurma() {
+        return turma;
+    }
+
+    public void setTurma(List<Turma> turma) {
+        this.turma = turma;
+    }
 }
