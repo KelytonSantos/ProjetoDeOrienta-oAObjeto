f58d1dd44c050238c8e0286aa5e90e5192a1b341
Lucas Santos <kelytonlucas@gmail.com>
2025-04-25 09:19:31 -0300

    feat:cleaning for new project

--- src/App.java
+++ src/App.java
@@ -1,17 +1,5 @@
-import model.Emprestimo;
-import model.Livro;
-import model.Usuario;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Usuario pessoa1 = new Usuario("Santos", "333.333.333-33", "lucassantos@lucas.com");
-        Livro livro = new Livro("1984", "George Orwell", 1949);
-        Emprestimo emp = new Emprestimo(livro, pessoa1);
-
-        System.out.println(emp.toString());
-        System.out.println(emp.emprestimo(9));
-        System.out.println(emp.toString());
-        System.out.println(emp.devolver());
-        System.out.println(emp.toString());
     }
 }
